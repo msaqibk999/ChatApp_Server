f5cd44f5d87c78279fd59211d08419de975e56e5
saqibk999 <saqib.mohd1@byjus.com>
2023-07-31 18:54:47 +0530

    first commit

---
+++ app/models/message.rb
@@ -0,0 +1,15 @@
+class Message < ApplicationRecord
+    validates :body, presence: true
+    validates :sender, presence: true, length: { minimum: 10 }
+    after_create_commit { broadcast_message }
+
+    private
+
+    def broadcast_message
+        ActionCable.server.broadcast( "MessagesChannel" ,{
+                                        id:,
+                                        sender:,
+                                        body:
+        })
+    end
+end
