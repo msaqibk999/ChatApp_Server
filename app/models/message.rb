96b0a912a6e903c7e7d1a5f0a1288d181b8d2f9a
saqibk999 <saqib.mohd1@byjus.com>
2023-08-12 17:27:58 +0530

    message model fix

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,15 +1,15 @@
 class Message < ApplicationRecord
     validates :body, presence: true
     validates :sender, presence: true, length: { minimum: 10 }
-    after_create_commit { broadcast_message }
-
+    after_create_commit :broadcast_message
+  
     private
-
+  
     def broadcast_message
-        ActionCable.server.broadcast( "MessagesChannel" ,{
-                                        id:,
-                                        sender:,
-                                        body:
-        })
+      ActionCable.server.broadcast("MessagesChannel", {
+        id: self.id,
+        sender: self.sender,
+        body: self.body
+      })
     end
-end
+  end
